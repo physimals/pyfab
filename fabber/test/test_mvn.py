a8bad5e344611ec0d5bed36e5e65aa4998bea3bc
Martin Craig <martin.craig@eng.ox.ac.uk>
2018-10-09 13:33:45 +0100

    Work in progress

--- fabber/test/test_mvn.py
+++ fabber/test/test_mvn.py
@@ -17,3 +17,27 @@ def test_three_params():
     mvn = MVN(d)
     assert mvn.nparams == 3
 
+def test_mean_index():
+    d = np.zeros((5, 5, 5, 10))
+    mvn = MVN(d)
+    assert mvn.mean_index(0) == 6
+    assert mvn.mean_index(1) == 7
+    assert mvn.mean_index(2) == 8
+
+def test_var_index():
+    d = np.zeros((5, 5, 5, 10))
+    mvn = MVN(d)
+    assert mvn.var_index(0) == 0
+    assert mvn.var_index(1) == 2
+    assert mvn.var_index(2) == 5
+
+def test_covar_index():
+    d = np.zeros((5, 5, 5, 10))
+    mvn = MVN(d)
+    assert mvn.var_index(0, 1) == 1
+    assert mvn.var_index(0, 2) == 3
+    assert mvn.var_index(1, 0) == 1
+    assert mvn.var_index(1, 2) == 4
+    assert mvn.var_index(2, 0) == 3
+    assert mvn.var_index(2, 1) == 4
+
