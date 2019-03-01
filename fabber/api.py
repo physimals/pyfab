d30b0670416e6bc8a67a0bf4e00aeb0a01a6efa6
Martin Craig <martin.craig@eng.ox.ac.uk>
2019-03-01 15:05:39 +0000

    Lowercase model groups for consistency

--- fabber/api.py
+++ fabber/api.py
@@ -90,14 +90,14 @@ def find_fabber(*extra_search_dirs):
         lib = _find_file(lib, search_dir, _LIB_FORMAT % "fabbercore_shared")
         lib_regex = re.compile(r'.*fabber_models_(.+)\..*')
         for model_lib in glob.glob(os.path.join(search_dir, _LIB_FORMAT % "fabber_models_*")):
-            model_name = lib_regex.match(model_lib).group(1)
-            model_libs[model_name] = model_libs.get(model_name, model_lib)
+            group_name = lib_regex.match(model_lib).group(1).lower()
+            model_libs[group_name] = model_libs.get(group_name, model_lib)
         exe_regex = re.compile(r'.*fabber_(.+)\.?.*')
         for model_exe in glob.glob(os.path.join(search_dir, _BIN_FORMAT % "fabber_*")):
-            model_name = exe_regex.match(model_exe).group(1)
-            if model_name != "var":
+            group_name = exe_regex.match(model_exe).group(1).lower()
+            if group_name != "var":
                 # Old executable which messes things up sometimes!
-                model_exes[model_name] = model_exes.get(model_name, model_exe)
+                model_exes[group_name] = model_exes.get(group_name, model_exe)
     
     return lib, exe, model_libs, model_exes
 
